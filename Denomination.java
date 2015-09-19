0b034ae14bb182c40145a0a280fd40669d9f3b67
Jack Kwan <jackkwan@172-16-210-88.DYNAPOOL.NYU.EDU>
2015-09-18 21:27:38 -0400

    anneal and random

---
+++ Denomination.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+
+public class Denomination {
+	static int NUM_PENCES = 240;
+	int[] coins;
+	int[] best;
+	
+	public Denomination() {
+		this.coins = generateRand();
+		best = new int[NUM_PENCES];
+		dp();
+	}
+	
+	public Denomination(int[] c) {
+		this.coins = c;
+		best = new int[NUM_PENCES];
+		dp();
+	}
+	
+	public void changeCoins(int[] newC) {
+		this.coins = newC;
+		dp();
+	}
+	
+	private static int[] generateRand() {
+		int[] result = {1,0,0,0,0,0,0};
+		for (int i = 1; i < result.length; i++) result[i] = 2+(int)(Math.random()*238);
+		return result;
+	}
+	
+	
+	//test
+	private void setBestAllTwos() {
+		for (int i = 1; i < NUM_PENCES; i++) {
+			best[i] = 2;
+		}
+		best[0] = 0;
+	}
+	
+	private void dp() {
+		for (int i = 0; i < NUM_PENCES; i++) best[i] = 0;
+		for (int i = 0; i < coins.length; i++) {
+			best[coins[i]] = 1;
+		}
+		best[0] = 0;
+		//System.out.println(Arrays.toString(best));
+		for (int i = 1; i < best.length; i++) {
+			int currBest = Integer.MAX_VALUE;
+			if (best[i] != 0) continue;
+			for (int j = 0; j < coins.length; j++) {
+				int currCoin = coins[j];
+				if (i <= currCoin) continue;
+				if (best[i-currCoin]+1 < currBest) {
+					currBest = best[i-currCoin]+1;
+				}
+			}
+			best[i] = currBest;
+		}
+	}
+	
+	public int score(int N) {
+		int result = 0;
+		for (int i = 1; i < best.length; i++) {
+			result += i%5==0 ? best[i]*N : best[i];
+		}
+		return result;
+	}	
+}
---
+++ Random.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Random {
+
+	static int N = 100;
+	
+	
+	public static void main(String[] args) {
+		//uncomment when ready
+		//N = Integer.parseInt(args[0]);
+		
+		double temp = 1000000;
+        double coolingRate = 0.00002;
+		Denomination best = new Denomination();
+		
+		System.out.println("Random: Initial solution score: " + best.score(N));
+	    System.out.println("Initial Result: " + Arrays.toString(best.coins)); 
+	    
+		while (temp > 1) {
+			//System.out.println(count);
+			Denomination curr = new Denomination();
+			if (curr.score(N) < best.score(N)) {
+				best = curr;
+			}
+			temp *= 1-coolingRate;
+		}
+		System.out.println("Final solution distance: " + best.score(N));
+        System.out.println("Tour: " + Arrays.toString(best.coins));   
+	}
+
+}
